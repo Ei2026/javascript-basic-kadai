d4aae6659be18f259ea905b2ecf005842ef7d84e
Ei2026 <sakata.a@shinmaywa.co.jp>
2026-04-13 13:23:26 +0900

    課題_20_提出

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+// HTML要素を取得して定数に代入
+const text = document.getElementById('text');
+const btn = document.getElementById('btn');
+
+// ボタンがクリックされたときに文字を変更
+btn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました';
+});
